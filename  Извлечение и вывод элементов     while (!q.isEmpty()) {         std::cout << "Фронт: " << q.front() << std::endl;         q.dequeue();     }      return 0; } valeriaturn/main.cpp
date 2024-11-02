8c6e2fdca8ae51087fb0667918dc17502cf9e7c5
valeriabukova <valeriasergeevna688@gmail.com>
2024-11-02 21:26:09 +0200

    Initial Commit

---
+++  Извлечение и вывод элементов     while (!q.isEmpty()) {         std::cout << "Фронт: " << q.front() << std::endl;         q.dequeue();     }      return 0; } valeriaturn/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//   Извлечение и вывод элементов     while (!q.isEmpty()) {         std::cout << "Фронт: " << q.front() << std::endl;         q.dequeue();     }      return 0; } valeriaturn
+//
+//  Created by Valeria  Bukova on 02.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
